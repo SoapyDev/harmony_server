d1cc9bacc5588be42daf2e1df0984437e3d6c9ed
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2024-04-18 08:01:20 -0400

    Initial state for server.
    Health check
    Definition of routes
    First test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use harmony_server::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    run(listener)?.await
 }
