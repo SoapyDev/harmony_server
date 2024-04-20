c0be505e987ee345c1e513dce2f6dfac30255041
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2024-04-20 19:38:16 -0400

    changing harmony_db to harmony-db

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -21,8 +21,8 @@ fi
 DB_USER=${POSTGRES_USER:=postgres}
 # Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD=${POSTGRES_PASSWORD:=password}
-# Check if a custom database name has been set, otherwise default to 'harmony_db'
-DB_NAME=${POSTGRES_DB:=harmony_db}
+# Check if a custom database name has been set, otherwise default to 'harmony-db'
+DB_NAME=${POSTGRES_DB:='harmony-db'}
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT=${POSTGRES_PORT:=6543}
 # Check if a custom host has been set, otherwise default to 'localhost'
