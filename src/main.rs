12be6689ed1383e610f2a8996bef01d6701ca24f
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2024-04-22 16:11:53 -0400

    Refactoring main & run for better configuration

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use harmony_server::configurations::get_configuration;
-use harmony_server::startup::run;
-use sqlx::PgPool;
-use std::net::TcpListener;
-
+use harmony_server::startup::Application;
 use harmony_server::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), anyhow::Error> {
     let subscriber = get_subscriber("harmony_server".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     // Get configuration file
-    let settings = get_configuration().expect("Failed to read configuration.");
-
-    let connection = PgPool::connect_lazy_with(settings.database.with_db());
-
-    let listener = TcpListener::bind(format!(
-        "{}:{}",
-        settings.application.host, settings.application.port
-    ))?;
-    run(listener, connection)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,63 @@
 use crate::authentication::reject_anonymous_users;
+
 use crate::routes::{allergy, beneficiary, health_check, login, message, transaction, user};
 use actix_web::dev::Server;
 use actix_web::{middleware::Compress, web, App, HttpServer};
 use actix_web_lab::middleware::from_fn;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use actix_session::SessionMiddleware;
 use tracing_actix_web::TracingLogger;
+use actix_session::storage::RedisSessionStore;
+use actix_web::cookie::Key;
+use secrecy::{ExposeSecret, Secret};
+use sqlx::postgres::PgPoolOptions;
+use crate::configurations::{DatabaseSettings, Settings};
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub struct Application{
+    port: u16,
+    server: Server,
+}
+
+impl Application{
+
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
+        
+        let connection_pool = get_connection_pool(&configuration.database).await.expect("Failed to connect to postgres");
+
+        let address = format!("{}:{}",configuration.application.host,configuration.application.port);
+
+        let listener = TcpListener::bind(address)?;
+
+        let port = listener.local_addr().unwrap().port();
+
+        let server = run(listener, connection_pool,configuration.application.hmac_secret, configuration.redis_uri).await?;
+
+
+        Ok(Self{port, server})
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(),std::io::Error>{
+        self.server.await
+    }
+
+}
+
+pub async fn get_connection_pool(configuration: &DatabaseSettings) -> Result<PgPool, sqlx::Error> {
+    PgPoolOptions::new()
+        .connect_with(configuration.with_db())
+        .await
+}
+
+
+pub async fn run(listener: TcpListener, db_pool: PgPool, hmac_secret: Secret<String>, redis_uri: Secret<String>) -> Result<Server, anyhow::Error> {
     let connection = web::Data::new(db_pool);
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
     let server = HttpServer::new(move || {
         App::new()
             .wrap(Compress::default())
+            .wrap(SessionMiddleware::new(redis_store.clone(),secret_key.clone()))
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check::health_check))
             .route("/login", web::post().to(login::login))
